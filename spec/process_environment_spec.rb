719a8d1c697a465d2d08824cc50abfc316030486
Max Meyer <dev@fedux.org>
2014-03-26 16:04:08 +0100

    Fixed a lot

--- spec/process_environment_spec.rb
+++ spec/process_environment_spec.rb
@@ -4,21 +4,41 @@ require 'spec_helper'
 describe ProcessEnvironment do
   context '#initialize' do
     it 'accepts an environment' do
+      env = double('ENV')
+      allow(env).to receive(:to_hash).and_return({})
+
       expect {
-        ProcessEnvironment.new(ENV.to_hash)
+        ProcessEnvironment.new(env)
       }.not_to raise_error
     end
   end
 
   context '#fetch' do
     it 'fetches environment variable' do
-      env = ProcessEnvironment.new(variable: 'value')
+      env = double('ENV')
+      allow(env).to receive(:to_hash).and_return({variable: 'value'})
+
+      env = ProcessEnvironment.new(env)
       expect(env.fetch(:variable)).to eq('value')
     end
 
     it 'returns an empty string if variable cannot be found' do
-      env = ProcessEnvironment.new({})
+      env = double('ENV')
+      allow(env).to receive(:to_hash).and_return({})
+
+      env = ProcessEnvironment.new(env)
       expect(env.fetch(:variable)).to eq('')
+    end
+  end
+
+  context '#write' do
+    it 'writes to environment' do
+      env = double('ENV')
+      allow(env).to receive(:to_hash).and_return({})
+      expect(env).to receive(:[]=).with('variable', 'value')
+
+      env = ProcessEnvironment.new(env)
+      env.write(:variable, 'value')
     end
   end
 end
